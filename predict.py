e3a8cef7ac5a66f5c6ba58f711c078f5914a9556
brookeojala <brooke.ojala@gmail.com>
2026-05-27 10:55:28 -0700

    finshed updates by brooke

--- predict.py
+++ predict.py
@@ -65,35 +65,17 @@ def score_settings(water_matrix, energy_matrix):
 
     return score_matrix, opt_fan_idx, opt_temp_idx
 
-def predict(curr_fan_sp, curr_dew_pt, curr_water_tp): #each input should only be a single value
-    #TODO --> is there a way to get this to only run once and be stored? maybe offload to another file
-    
-    # Training data (always in the file) --> to be replaced
-    df_train = pd.read_csv("Cooling chamber data.xlsx - Extrapolated data.csv")
+def predict(curr_fan_sp, curr_chiller_tp, curr_amb_dew_pt, curr_amb_rh, curr_coil_in_temp):
 
-    #Projection data --> inputted from outside
+    # 1. Load trained models
+    water_model = joblib.load("water_model.joblib")
+    energy_model = joblib.load("energy_model.joblib")
 
-    split_index = int(len(df_train) * 0.8)
+    # 2. Define grid search bounds (higher resolution works great with 2D!)
+    fan_steps = np.linspace(0, 5, 5)    # 0 to 5 with 5 variations
+    temp_steps = np.linspace(0, 15, 15) #0 to 15 with 15 variations
 
-    test = df_train.iloc[:split_index]
-    train = df_train.iloc[split_index:]
-    
-    # Train models
-    water_model, energy_model = train_regression_models(df_train,train,test)
-
-
-    # Predict water output and energy yeild at given params
-
-    X_pred = np.array([[curr_dew_pt, curr_water_tp]]) #later add fan speed here
-    Y_pred_water = water_model.predict(X_pred)
-    Y_pred_energy = energy_model.predict(X_pred)
-
-    # Grid inputs
-    #fan_steps = np.linspace(1, 5, 10) #1-5 with 10 steps
-    temp_steps = np.linspace(5, 10, 10) 
-    dew_point = curr_dew_pt #current input from the service
-
-    # Grid search
+    # 3. Perform 2D grid search
     water_mat, energy_mat = grid_search(
         (water_model, energy_model), 
         curr_amb_dew_pt, 
@@ -131,5 +113,5 @@ def predict(curr_fan_sp, curr_dew_pt, curr_water_tp): #each input should only be
     print(f"Maximized System Yield:    {op_water_pred:.2f} ml/hr")
     print(f"Minimized 20 min Power Draw:    {op_energy_pred:.2f} kWh")
     
-    result  = (op_water_temp, op_fan_sp, curr_water_pred, curr_energy_pred)
+    result = (op_water_temp, op_fan_sp, curr_water_pred, curr_energy_pred)
     return result
